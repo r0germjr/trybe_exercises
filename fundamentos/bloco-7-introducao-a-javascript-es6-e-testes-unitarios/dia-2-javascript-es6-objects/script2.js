83ea9db3a0193e7a1f67837d96324fff6da7b58d
r0ger <rogeriomaiajr13@gmail.com>
2021-07-06 02:02:51 -0300

    Exercise 6 solved

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
@@ -20,9 +20,18 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 
-// Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . 
-// Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 console.log(allLessons);
+
+const getStudentsTotal = (lessons) => {
+  const keys = Object.keys(lessons);
+  let totalStudants = 0;
+  for(index = 0; index < keys.length; index +=1) {
+    const currentKey = keys[index];
+    totalStudants += lessons[currentKey].numeroEstudantes;
+  }
+  return totalStudants;
+}
+console.log(getStudentsTotal(allLessons));
