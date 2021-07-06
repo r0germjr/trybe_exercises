016368e3bfe94a4ee0ea505d1e92e982e8803c19
r0ger <rogeriomaiajr13@gmail.com>
2021-07-06 01:41:27 -0300

    Exercise 6 solved

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
@@ -20,24 +20,9 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, 
-// sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 
+// Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . 
+// Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
-const newKey = (object, key, value) => {
-  object[key] = value;
-};
-
-newKey(lesson2, 'turno', 'manhã');
-console.log(lesson2);
-
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-const logKeys = (object) => Object.keys(object);
-console.log(logKeys(lesson3));
-
-// Crie uma função para mostrar o tamanho de um objeto.
-const objLength = (object) => console.log(Object.keys(object).length);
-
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
-
-const valuesLog = (object) => console.log(Object.values(object));
-valuesLog(lesson3);
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
