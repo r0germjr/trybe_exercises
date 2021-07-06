ddf34c1b9986ac6a26d20d9d1fa2cea859c24e49
r0ger <rogeriomaiajr13@gmail.com>
2021-07-06 02:09:40 -0300

    Exercise 7 solved

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script2.js
@@ -20,18 +20,7 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 
-const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
-console.log(allLessons);
-
-const getStudentsTotal = (lessons) => {
-  const keys = Object.keys(lessons);
-  let totalStudants = 0;
-  for(index = 0; index < keys.length; index +=1) {
-    const currentKey = keys[index];
-    totalStudants += lessons[currentKey].numeroEstudantes;
-  }
-  return totalStudants;
-}
-console.log(getStudentsTotal(allLessons));
+const valueByPosition = (object, position) => console.log(Object.values(object)[position]);
+valueByPosition(lesson3, 3);
