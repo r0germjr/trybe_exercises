32014320f5eb3ff2efbb27b37b10eb6231224a12
Rogério Maia Jr. [Roger] <rogeriomaiajr13@gmail.com>
2021-08-12 02:29:05 -0300

    Exercise 2 ok

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -6,8 +6,6 @@ const arrays = [
   [4, 5, 6],
 ];
 
-function flatten() {
-  return arrays.reduce((acc, curr) => acc.concat(curr),[]);
-}
+const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr),[]);
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
